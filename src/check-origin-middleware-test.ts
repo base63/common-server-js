438ace3d31b408286579b9285dc4ef46742475a3
Horia Coman <horia141@gmail.com>
2017-10-12 18:45:55 +0300

    Finish tests for the check origin middleware

--- src/check-origin-middleware-test.ts
+++ src/check-origin-middleware-test.ts
@@ -1,13 +1,9 @@
-import { use, expect } from 'chai'
+import { expect } from 'chai'
+import * as HttpStatus from 'http-status-codes'
 import 'mocha'
 import * as td from 'testdouble'
 
 import { newCheckOriginMiddleware } from './check-origin-middleware'
-
-const testdoubleChai = require('testdouble-chai');
-
-
-use(testdoubleChai(td));
 
 
 describe('CheckOriginMiddleware', () => {
@@ -23,5 +19,39 @@ describe('CheckOriginMiddleware', () => {
         checkOriginMiddleware(mockReq as any, mockRes as any, () => { passedCheck = true });
 
         expect(passedCheck).to.be.true;
+    });
+
+    it('should allow request with proper origin out of multiple ones', () => {
+        const checkOriginMiddleware = newCheckOriginMiddleware(['base63.com', 'base63.io']);
+        var passedCheck = false;
+
+        const mockReq = td.object(['header']);
+        const mockRes = td.object('Response');
+
+        td.when(mockReq.header('Origin')).thenReturn('base63.com');
+
+        checkOriginMiddleware(mockReq as any, mockRes as any, () => { passedCheck = true });
+
+        expect(passedCheck).to.be.true;
+    });
+
+    it('should block a request with a disallowed origin', () => {
+        const checkOriginMiddleware = newCheckOriginMiddleware(['base63.com']);
+        var passedCheck = false;
+
+        const mockReq = td.object({
+            header: (_name: string) => {},
+            log: {warn: (_msg: string) => {}}
+        });
+        const mockRes = td.object(['status', 'end']);
+
+        td.when(mockReq.header('Origin')).thenReturn('base63.io');
+
+        checkOriginMiddleware(mockReq as any, mockRes as any, () => { passedCheck = true });
+
+        expect(passedCheck).to.be.false;
+        td.verify(mockReq.log.warn('Origin is not allowed'));
+        td.verify(mockRes.status(HttpStatus.BAD_REQUEST));
+        td.verify(mockRes.end());
     });
 });
