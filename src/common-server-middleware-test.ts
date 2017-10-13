2c2fbfcaa0293350565b20d74ecade4298d6bc06
Horia Coman <horia141@gmail.com>
2017-10-13 18:20:32 +0300

    Better structure to the middleware tests

--- src/common-server-middleware-test.ts
+++ src/common-server-middleware-test.ts
@@ -4,13 +4,14 @@ import * as td from 'testdouble'
 
 import { Env } from '@base63/common-js'
 
-import { newLocalCommonServerMiddleware } from './common-server-middleware'
+import { newLocalCommonServerMiddleware, newCommonServerMiddleware } from './common-server-middleware'
 
 
 describe('LocalCommonServerMiddleware', () => {
     it('should create a completed request object', () => {
         const serverMiddleware = newLocalCommonServerMiddleware('base63', Env.Local);
 
+        const rightNow = new Date(Date.now());
         var passedCheck = false;
 
         const mockReq = td.object({
@@ -25,6 +26,35 @@ describe('LocalCommonServerMiddleware', () => {
 
         expect(passedCheck).to.be.true;
         expect(mockReq.requestTime).to.be.not.null;
+        expect((mockReq as any).requestTime.getTime()).to.be.gte(rightNow.getTime());
+        expect((mockReq as any).requestTime.getTime()).to.be.approximately(rightNow.getTime(), 10);
+        expect(mockReq.log).to.be.not.null;
+        expect(mockReq.errorLog).to.be.not.null;
+    });
+});
+
+
+describe('CommonServerMiddleware', () => {
+    it('should create a completed request object', () => {
+        const serverMiddleware = newCommonServerMiddleware('base63', Env.Prod, 'BAD', 'BAD', 'BAD');
+
+        const rightNow = new Date(Date.now());
+        var passedCheck = false;
+
+        const mockReq = td.object({
+            requestTime: null,
+            log: null,
+            errorLog: null,
+            header: () => {}
+        });
+        const mockRes = td.object(['on']);
+
+        serverMiddleware(mockReq as any, mockRes as any, () => { passedCheck = true });
+
+        expect(passedCheck).to.be.true;
+        expect(mockReq.requestTime).to.be.not.null;
+        expect((mockReq as any).requestTime.getTime()).to.be.gte(rightNow.getTime());
+        expect((mockReq as any).requestTime.getTime()).to.be.approximately(rightNow.getTime(), 10);
         expect(mockReq.log).to.be.not.null;
         expect(mockReq.errorLog).to.be.not.null;
     });
