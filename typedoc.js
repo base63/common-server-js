7b3fae00632a27e9e572f98b5c4a97f3e58d3060
Horia Coman <horia141@gmail.com>
2017-10-16 15:38:03 +0300

    Add docs generation.

---
+++ typedoc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "name": "common-server-js",
+    "mode": "modules",
+    "module": "common-server-js",
+    "exclude": "**/*-test.ts",
+    "out": "docs",
+    "excludePrivate": true
+}
